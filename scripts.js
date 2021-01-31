9d75c565ea09fb160c41852d49417e6d50939422
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-01-30 22:10:10 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -16,3 +16,53 @@ const Modal = {
             .remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Criação WebSite',
+        amount: 500000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    },
+]
+
+const Transaction = {
+    incomes(){
+        // somar as entradas
+    },
+    expenses(){
+        // somar as saídas
+    },
+    total(){
+        // entradas - saídas
+    }
+}
+
+// Substituir os dados do HTML com os dados do JavaScript
+const DOM = {
+    innerHTMLTransaction() {
+
+        const html = `
+        <tr>
+            <td class="description">Luz</td>
+            <td class="expense">- R$ 500,00</td>
+            <td class="date">23/01/2021</td>
+            <td>                        
+                <img src="./assets/minus.svg" alt="Remover Transação">
+            </td>
+        </tr>
+        `
+    }
+}
